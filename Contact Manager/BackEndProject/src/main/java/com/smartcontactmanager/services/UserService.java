94fe7124bc6515c120f148443e75e7fe433e1a03
ssonje <ssonje99@gmail.com>
2022-09-23 23:32:52 +0530

    Rename User services

--- Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.smartcontactmanager.services;
-
-import com.smartcontactmanager.entities.User;
-
-public interface UserService {
-
-	public User addUser(User user);
-
-}
---
+++ Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/user/signup/services/UserSignUpService.java
@@ -0,0 +1,9 @@
+package com.smartcontactmanager.user.signup.services;
+
+import com.smartcontactmanager.entities.User;
+
+public interface UserSignUpService {
+
+	public User addUser(User user);
+
+}
