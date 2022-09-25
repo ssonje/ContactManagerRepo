50ce6f1394a3ac592f9f7e098bfe6936cc59afa5
ssonje <ssonje99@gmail.com>
2022-09-25 23:07:33 +0530

    Added UI Folder for the Remaining Components

--- Contact Manager/FrontEndProject/src/Components/UI Components/About/About.js
+++ Contact Manager/FrontEndProject/src/Components/UI Components/About/About.js
@@ -1,20 +1,16 @@
-import AboutCss from "./CSS/About.module.css";
+import AboutUI from "./UI/AboutUI";
 import CustomNavbar from "../Navbar/CustomNavbar";
 import React from "react";
 
 /**
  * @Component
- * `About` component provides the UI for About Section in Contact Manager Appication.
+ * `About` component provides the UI and functionality for About Section in Contact Manager Appication.
  */
 const About = () => {
     return (
         <div>
             <CustomNavbar currentLocation="/about"></CustomNavbar>
-            <div className={"d-flex align-items-center justify-content-center " + (AboutCss.ContainerWindow)}>
-                <div className={(AboutCss.AboutText)}>
-                    <h1>About us</h1>
-                </div>
-            </div>
+            <AboutUI />
         </div>
     );
 }
