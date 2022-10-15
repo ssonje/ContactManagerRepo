1c6315a9fb0554fe6dc0426bfe702b2635de1638
ssonje <ssonje99@gmail.com>
2022-10-15 20:00:53 +0530

    Added Service Layer to back end code

--- Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/controllers/UserController.java
+++ Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.smartcontactmanager.controllers;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -14,87 +13,45 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.smartcontactmanager.dao.ContactRepository;
-import com.smartcontactmanager.dao.UserRepository;
 import com.smartcontactmanager.entities.Contact;
 import com.smartcontactmanager.entities.User;
+import com.smartcontactmanager.services.UserControllerService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
 	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private ContactRepository contactRepository;
+	private UserControllerService userControllerService;
 
 	@GetMapping("/profile")
 	public User userProfile(Principal principal) {
-		return userRepository.loadUserByEmail(principal.getName());
+		return userControllerService.userProfile(principal);
 	}
 
 	@PostMapping("/add/contact")
 	public Contact addContact(@Valid @RequestBody Contact contact, Principal principal) {
-		User user = userRepository.loadUserByEmail(principal.getName());
-		contact.setUser(user);
-		contactRepository.save(contact);
-		return contact;
+		return userControllerService.addContact(contact, principal);
 	}
 
 	@GetMapping("/view/contacts")
 	public List<Contact> viewContacts(Principal principal) {
-		User user = userRepository.loadUserByEmail(principal.getName());
-		List<Contact> contacts = contactRepository.findContactsByUserID(user.getId());
-		return contacts;
+		return userControllerService.viewContacts(principal);
 	}
 
 	@GetMapping("/view/contact/{id}")
 	public Contact getContactByID(@PathVariable("id") Integer id, Principal principal) {
-		User user = userRepository.loadUserByEmail(principal.getName());
-		Optional<Contact> contactOptional = contactRepository.findById(id);
-		Contact contact = contactOptional.get();
-
-		if (user.getId() == contact.getUser().getId()) {
-			return contact;
-		}
-
-		return null;
+		return userControllerService.getContactByID(id, principal);
 	}
 
 	@PostMapping("/delete/contact/{id}")
 	public Contact deleteContact(@PathVariable("id") Integer id, Principal principal) {
-		User user = userRepository.loadUserByEmail(principal.getName());
-		Optional<Contact> contactOptional = contactRepository.findById(id);
-		Contact contact = contactOptional.get();
-
-		if (user.getId() == contact.getUser().getId()) {
-			contactRepository.deleteById(id);
-			return contact;
-		}
-
-		return null;
+		return userControllerService.deleteContact(id, principal);
 	}
 
 	@PostMapping("/modify/contact/{id}")
 	public Contact modifyContact(@PathVariable("id") Integer id, Principal principal, @Valid @RequestBody Contact contact) {
-		User user = userRepository.loadUserByEmail(principal.getName());
-		Optional<Contact> contactOptional = contactRepository.findById(id);
-		Contact contactFromID = contactOptional.get();
-
-		if ((user.getId() == contactFromID.getUser().getId()) && (contactFromID.getId() == contact.getId())) {
-			contactFromID.setDescription(contact.getDescription());
-			contactFromID.setEmail(contact.getEmail());
-			contactFromID.setImageURL(contact.getImageURL());
-			contactFromID.setMobileNumber(contact.getMobileNumber());
-			contactFromID.setName(contact.getName());
-			contactFromID.setNickname(contact.getNickname());
-			contactFromID.setWork(contact.getWork());
-			contactRepository.save(contactFromID);
-			return contactFromID;
-		}
-
-		return null;
+		return userControllerService.modifyContact(id, principal, contact);
 	}
 
 }
---
+++ Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/services/UserControllerServiceImpl.java
@@ -0,0 +1,92 @@
+package com.smartcontactmanager.services;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.smartcontactmanager.dao.ContactRepository;
+import com.smartcontactmanager.dao.UserRepository;
+import com.smartcontactmanager.entities.Contact;
+import com.smartcontactmanager.entities.User;
+
+@Service
+public class UserControllerServiceImpl implements UserControllerService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private ContactRepository contactRepository;
+
+	@Override
+	public User userProfile(Principal principal) {
+		return userRepository.loadUserByEmail(principal.getName());
+	}
+
+	@Override
+	public Contact addContact(Contact contact, Principal principal) {
+		User user = userRepository.loadUserByEmail(principal.getName());
+		contact.setUser(user);
+		contactRepository.save(contact);
+		return contact;
+	}
+
+	@Override
+	public List<Contact> viewContacts(Principal principal) {
+		User user = userRepository.loadUserByEmail(principal.getName());
+		List<Contact> contacts = contactRepository.findContactsByUserID(user.getId());
+		return contacts;
+	}
+
+	@Override
+	public Contact getContactByID(Integer id, Principal principal) {
+		User user = userRepository.loadUserByEmail(principal.getName());
+		Optional<Contact> contactOptional = contactRepository.findById(id);
+		Contact contact = contactOptional.get();
+
+		if (user.getId() == contact.getUser().getId()) {
+			return contact;
+		}
+
+		return null;
+	}
+
+	@Override
+	public Contact deleteContact(Integer id, Principal principal) {
+		User user = userRepository.loadUserByEmail(principal.getName());
+		Optional<Contact> contactOptional = contactRepository.findById(id);
+		Contact contact = contactOptional.get();
+
+		if (user.getId() == contact.getUser().getId()) {
+			contactRepository.deleteById(id);
+			return contact;
+		}
+
+		return null;
+	}
+
+	@Override
+	public Contact modifyContact(Integer id, Principal principal, Contact contact) {
+		User user = userRepository.loadUserByEmail(principal.getName());
+		Optional<Contact> contactOptional = contactRepository.findById(id);
+		Contact contactFromID = contactOptional.get();
+
+		if ((user.getId() == contactFromID.getUser().getId()) && (contactFromID.getId() == contact.getId())) {
+			contactFromID.setDescription(contact.getDescription());
+			contactFromID.setEmail(contact.getEmail());
+			contactFromID.setImageURL(contact.getImageURL());
+			contactFromID.setMobileNumber(contact.getMobileNumber());
+			contactFromID.setName(contact.getName());
+			contactFromID.setNickname(contact.getNickname());
+			contactFromID.setWork(contact.getWork());
+			contactRepository.save(contactFromID);
+			return contactFromID;
+		}
+
+		return null;
+	}
+
+}
