2e5973cdbdfbe905ca46abc1c919f0c1ccbae547
ssonje <ssonje99@gmail.com>
2022-10-16 15:12:42 +0530

    Added Toast Message for Fetching User Information

--- Contact Manager/FrontEndProject/src/Components/UI Components/UserProfile/Database Services/FetchUserInformation.js
+++ Contact Manager/FrontEndProject/src/Components/UI Components/UserProfile/Database Services/FetchUserInformation.js
@@ -1,5 +1,6 @@
 import { BASE_URL } from "../../../../Constants/BackEndServerURL";
 import { HTTPStatusErrorHelper } from "../../../Database Service Components/Helpers/HTTPStatusErrorHelper";
+import { toast } from "react-toastify";
 
 import axios from "axios";
 
@@ -19,6 +20,7 @@ export const FetchUserInformation = (setUser) => {
         }).then(
             (response) => {
                 // Successfully authenticated user
+                toast.success("User information fetched successfully!");
                 setUser(response.data);
             },
             (error) => {
