8f2bde9b216136525e54a82c297e8f0c0ea48fb6
ssonje <ssonje99@gmail.com>
2022-11-02 11:01:26 +0530

    FrontEnd-SpinnerUI

---
+++ Contact Manager/FrontEndProject/src/Components/UI Components/Loading Spinner/LoadingSpinner.js
@@ -0,0 +1,15 @@
+import LoadingSpinnerUI from "./UI/LoadingSpinnerUI";
+import React from "react";
+
+/**
+ * @Component
+ * `LoadingSpinner` component provides the UI and Funtionality for showing Loading animation while fetching information from the API's.
+ */
+const LoadingSpinner = () => {
+
+    return (
+        <LoadingSpinnerUI />
+    );
+}
+
+export default LoadingSpinner;
