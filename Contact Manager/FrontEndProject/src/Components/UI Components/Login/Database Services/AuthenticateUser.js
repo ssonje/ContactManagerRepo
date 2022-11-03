ec22a6f95dca8698b2896c67956d1a63ff443941
ssonje <ssonje99@gmail.com>
2022-11-03 09:38:59 +0530

    FrontEnd-LoadingSpinner-AuthenticateUser

--- Contact Manager/FrontEndProject/src/Components/UI Components/Login/Database Services/AuthenticateUser.js
+++ Contact Manager/FrontEndProject/src/Components/UI Components/Login/Database Services/AuthenticateUser.js
@@ -6,12 +6,20 @@ import axios from "axios";
 /**
  * @component
  * `AuthenticateUser` provides the functionality to authenticate the user by using the `axios`.
+ * @param {navigate} navigate
+ * Navigate to the destination where we want to go after authenticating the user.
+ * @param {setIsAPICalled} setIsAPICalled
+ * This function is used to set the correct value to the `isAPICalled`.
  * @param {username} username
  * Username to be authenticated
  * @param {password} password
  * Password for above username to be authenticated
  */
-export const AuthenticateUser = (navigate, username, password) => {
+export const AuthenticateUser = (navigate, setIsAPICalled, username, password) => {
+
+    // Set the `isAPICalled` to true as we've started calling the Database API
+    setIsAPICalled(true);
+
     return (
         axios.post(`${BASE_URL}/auth/login`, {
             username,
@@ -24,6 +32,9 @@ export const AuthenticateUser = (navigate, username, password) => {
 
                     // TODO: Dynamically open the profile of user as per the ROLE assigned to him/her.
                     navigate("/user/profile");
+
+                    // Set the `isAPICalled` to false as API is called successfully and we want to show the result
+                    setIsAPICalled(false);
                 }
 
                 return response.data;
@@ -31,6 +42,9 @@ export const AuthenticateUser = (navigate, username, password) => {
             (error) => {
                 // Show error while authenticating the user
                 HTTPStatusErrorHelper(error.response.status);
+
+                // Set the `isAPICalled` to false as API is called successfully and we want to show the result
+                setIsAPICalled(false);
             }
         )
     );
--- Contact Manager/FrontEndProject/src/Components/UI Components/Login/UI/LoginUI.js
+++ Contact Manager/FrontEndProject/src/Components/UI Components/Login/UI/LoginUI.js
@@ -8,6 +8,7 @@ import { useUserFormActionErrors } from "../../../Helpers/Hooks/useUserFormActio
 
 import BasAppCss from "../../../../CSS/BaseApp.module.css";
 import CustomNavbar from "../../Navbar/CustomNavbar";
+import LoadingSpinner from "../../Loading Spinner/LoadingSpinner";
 import LoginCss from "../CSS/Login.module.css";
 import React from "react";
 
@@ -23,6 +24,7 @@ import * as LoginFormPlaceholderConstants from "../Constants/LoginFormPlaceholde
 const LoginUI = () => {
 
     const [sideBarForProfileUI, setSideBarForProfileUI] = useState(false);
+    const [isAPICalled, setIsAPICalled] = useState(false);
 
     const [user, setUser] = useState({
         email: null,
@@ -41,7 +43,7 @@ const LoginUI = () => {
     // Authenticate and Login user to the Contact Manager Applciation
     const loginUser = (user) => {
         if (window.confirm("Are you sure you want to Login?")) {
-            AuthenticateUser(navigate, user.email, user.password);
+            AuthenticateUser(navigate, setIsAPICalled, user.email, user.password);
         }
     };
 
@@ -63,104 +65,110 @@ const LoginUI = () => {
             >
             </CustomNavbar>
             <div className={"d-flex align-items-center justify-content-center " + (sideBarForProfileUI ? BasAppCss.ContainerWindowForSideBarOn : BasAppCss.ContainerWindowForSideBarOff)}>
-                <Container>
-                    <Row>
-                        <Col md={4}></Col>
-                        <Col md={4}>
-                            <Form>
-                                <Container>
-                                    <h2 className={(LoginCss.LoginText) + " mb-4"}>Login</h2>
-                                    <Row>
-                                        <Col>
-                                            { /* User Email */}
-                                            <FormGroup>
-                                                <Label for={LoginFormFieldIDConstants.LOGIN_EMAIL_FIELD_ID} style={{ color: "#ffffff" }}>Email</Label>
-                                                <Input
-                                                    style={{ border: "none", boxShadow: "none" }}
-                                                    id={LoginFormFieldIDConstants.LOGIN_EMAIL_FIELD_ID}
-                                                    name={LoginFormInputNameConstants.LOGIN_EMAIL_INPUT_NAME}
-                                                    placeholder={LoginFormPlaceholderConstants.LOGIN_EMAIL_PLACEHOLDER}
-                                                    type="email"
-                                                    onChange={(e) => {
-                                                        setUser({ ...user, email: e.target.value });
-                                                    }}
-                                                />
-                                                <Label style={{ color: 'red', marginTop: 5 }}>{userLoginErrors.email}</Label>
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
+                {
+                    isAPICalled
+                        ?
+                            <LoadingSpinner sideBarForProfileUI={sideBarForProfileUI} />
+                        :
+                            <Container>
+                                <Row>
+                                    <Col md={4}></Col>
+                                    <Col md={4}>
+                                        <Form>
+                                            <Container>
+                                                <h2 className={(LoginCss.LoginText) + " mb-4"}>Login</h2>
+                                                <Row>
+                                                    <Col>
+                                                        { /* User Email */}
+                                                        <FormGroup>
+                                                            <Label for={LoginFormFieldIDConstants.LOGIN_EMAIL_FIELD_ID} style={{ color: "#ffffff" }}>Email</Label>
+                                                            <Input
+                                                                style={{ border: "none", boxShadow: "none" }}
+                                                                id={LoginFormFieldIDConstants.LOGIN_EMAIL_FIELD_ID}
+                                                                name={LoginFormInputNameConstants.LOGIN_EMAIL_INPUT_NAME}
+                                                                placeholder={LoginFormPlaceholderConstants.LOGIN_EMAIL_PLACEHOLDER}
+                                                                type="email"
+                                                                onChange={(e) => {
+                                                                    setUser({ ...user, email: e.target.value });
+                                                                }}
+                                                            />
+                                                            <Label style={{ color: 'red', marginTop: 5 }}>{userLoginErrors.email}</Label>
+                                                        </FormGroup>
+                                                    </Col>
+                                                </Row>
 
-                                    <Row>
-                                        <Col>
-                                            { /* User Password */}
-                                            <FormGroup>
-                                                <Label for={LoginFormFieldIDConstants.LOGIN_PASSWORD_FIELD_ID} style={{ color: "#ffffff" }}>Password</Label>
-                                                <InputGroup>
-                                                    <Input
-                                                        style={{ border: "none", boxShadow: "none" }}
-                                                        id={LoginFormFieldIDConstants.LOGIN_PASSWORD_FIELD_ID}
-                                                        name={LoginFormInputNameConstants.LOGIN_PASSWORD_INPUT_NAME}
-                                                        placeholder={LoginFormPlaceholderConstants.LOGIN_PASSWORD_PLACEHOLDER}
-                                                        type={showEnteredPassword ? "text" : "password"}
-                                                        onChange={(e) => {
-                                                            setUser({ ...user, password: e.target.value });
-                                                        }}
-                                                    />
-                                                    <Button style={{ background: "#ffffff", border: "none", boxShadow: "none" }}
-                                                        onClick={() => {
-                                                            handleShowEnteredPasswordButton();
-                                                        }}>
-                                                        <i style={{ color: "#000000" }}>
-                                                            {
-                                                                showEnteredPassword
-                                                                    ? <AiIcons.AiOutlineEye />
-                                                                    : <AiIcons.AiOutlineEyeInvisible />
-                                                            }
-                                                        </i>
-                                                    </Button>
-                                                </InputGroup>
-                                                <Label style={{ color: 'red', marginTop: 5 }}>{userLoginErrors.password}</Label>
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
+                                                <Row>
+                                                    <Col>
+                                                        { /* User Password */}
+                                                        <FormGroup>
+                                                            <Label for={LoginFormFieldIDConstants.LOGIN_PASSWORD_FIELD_ID} style={{ color: "#ffffff" }}>Password</Label>
+                                                            <InputGroup>
+                                                                <Input
+                                                                    style={{ border: "none", boxShadow: "none" }}
+                                                                    id={LoginFormFieldIDConstants.LOGIN_PASSWORD_FIELD_ID}
+                                                                    name={LoginFormInputNameConstants.LOGIN_PASSWORD_INPUT_NAME}
+                                                                    placeholder={LoginFormPlaceholderConstants.LOGIN_PASSWORD_PLACEHOLDER}
+                                                                    type={showEnteredPassword ? "text" : "password"}
+                                                                    onChange={(e) => {
+                                                                        setUser({ ...user, password: e.target.value });
+                                                                    }}
+                                                                />
+                                                                <Button style={{ background: "#ffffff", border: "none", boxShadow: "none" }}
+                                                                    onClick={() => {
+                                                                        handleShowEnteredPasswordButton();
+                                                                    }}>
+                                                                    <i style={{ color: "#000000" }}>
+                                                                        {
+                                                                            showEnteredPassword
+                                                                                ? <AiIcons.AiOutlineEye />
+                                                                                : <AiIcons.AiOutlineEyeInvisible />
+                                                                        }
+                                                                    </i>
+                                                                </Button>
+                                                            </InputGroup>
+                                                            <Label style={{ color: 'red', marginTop: 5 }}>{userLoginErrors.password}</Label>
+                                                        </FormGroup>
+                                                    </Col>
+                                                </Row>
 
-                                    <Row>
-                                        <Col>
-                                            { /* Navigate to the SignUp Page */}
-                                            <Container className="text-center">
-                                                <Label className={LoginCss.LoginText}>Do you want to Sign Up? <Link to="/signup">Signup here</Link>
-                                                </Label>
+                                                <Row>
+                                                    <Col>
+                                                        { /* Navigate to the SignUp Page */}
+                                                        <Container className="text-center">
+                                                            <Label className={LoginCss.LoginText}>Do you want to Sign Up? <Link to="/signup">Signup here</Link>
+                                                            </Label>
+                                                        </Container>
+                                                    </Col>
+                                                </Row>
+
+                                                <Row>
+                                                    <Col>
+                                                        { /* Navigate to the Forgot Password Page */}
+                                                        <Container className="text-center">
+                                                            <Label>
+                                                                <Link to="/forgot/password/email">Forgot Password</Link>
+                                                            </Label>
+                                                        </Container>
+                                                    </Col>
+                                                </Row>
+
+                                                <Row>
+                                                    <Col>
+                                                        { /* User Submit */}
+                                                        <Container className="text-center">
+                                                            <Button className="btn btn-outline-light m-3" outline onClick={(e) => {
+                                                                handleForm(e);
+                                                            }}>Login</Button>
+                                                        </Container>
+                                                    </Col>
+                                                </Row>
                                             </Container>
-                                        </Col>
-                                    </Row>
-
-                                    <Row>
-                                        <Col>
-                                            { /* Navigate to the Forgot Password Page */}
-                                            <Container className="text-center">
-                                                <Label>
-                                                    <Link to="/forgot/password/email">Forgot Password</Link>
-                                                </Label>
-                                            </Container>
-                                        </Col>
-                                    </Row>
-
-                                    <Row>
-                                        <Col>
-                                            { /* User Submit */}
-                                            <Container className="text-center">
-                                                <Button className="btn btn-outline-light m-3" outline onClick={(e) => {
-                                                    handleForm(e);
-                                                }}>Login</Button>
-                                            </Container>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </Form>
-                        </Col>
-                        <Col md={4}></Col>
-                    </Row>
-                </Container>
+                                        </Form>
+                                    </Col>
+                                    <Col md={4}></Col>
+                                </Row>
+                            </Container>
+                }
             </div>
         </div>
     );
