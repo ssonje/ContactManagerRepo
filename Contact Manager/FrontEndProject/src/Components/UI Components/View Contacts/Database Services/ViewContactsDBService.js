c9a35ed77a13ae77a0025aa1a344ac2776aae983
ssonje <ssonje99@gmail.com>
2022-11-07 08:56:57 +0530

    FrontEnd-PaginationSupport

--- Contact Manager/FrontEndProject/src/Components/UI Components/View Contacts/Database Services/ViewContactsDBService.js
+++ Contact Manager/FrontEndProject/src/Components/UI Components/View Contacts/Database Services/ViewContactsDBService.js
@@ -10,12 +10,14 @@ import * as ToastWarningMessages from "../../../../Constants/Toast Messages/Toas
 /**
  * @component
  * `ViewContactsDBService` provides the functionality for viewing all the contacts for the user by using the `axios`.
- * @param {setContacts} setContacts
- * Fetched contacts are now passed to the setContacts function.
+ * @param {setContactContent} setContactContent
+ * Fetched contacts content are now passed to the `setContactContent` function.
  * @param {setIsAPICalled} setIsAPICalled
  * This function is used to set the correct value to the `isAPICalled`.
+ * @param {pageNumber} pageNumber
+ * The data for this `pageNumber` will be loaded.
  */
-const ViewContactsDBService = (setContacts, setIsAPICalled) => {
+const ViewContactsDBService = (setContactContent, setIsAPICalled, pageNumber) => {
 
     const authToken = JSON.parse(localStorage.getItem(localStorage.key(0)));
 
@@ -23,16 +25,23 @@ const ViewContactsDBService = (setContacts, setIsAPICalled) => {
     setIsAPICalled(true);
 
     return (
-        axios.get(`${BASE_URL}/user/view/contacts`, {
+        axios.get(`${BASE_URL}/user/view/contacts?pageNumber=${pageNumber}`, {
             headers: {
                 Authorization: 'Bearer ' + authToken
             }
         }).then(
             (response) => {
-                // Successfully fetched the contacts
-                setContacts(response.data.content);
+                // Successfully fetched the contacts content
+                setContactContent({
+                    content: response.data.content,
+                    totalPages: response.data.totalPages,
+                    totalElements: response.data.totalElements,
+                    pageSize: response.data.pageable.size,
+                    pageNumber: response.data.pageable.pageNumber,
+                    lastPage: response.data.last
+                });
 
-                if (response.data.length === 0) {
+                if (response.data.content.length === 0) {
                     toast.warning(ToastWarningMessages.TOAST_WARNING_NO_CONTACTS_FOUND);
                 } else {
                     toast.success(ToastSuccessMessages.TOAST_SUCCESS_FOR_SUCCESSFULLY_FETCHING_CONTACTS);
@@ -40,6 +49,9 @@ const ViewContactsDBService = (setContacts, setIsAPICalled) => {
 
                 // Set the `isAPICalled` to false as API is called successfully and we want to show the result
                 setIsAPICalled(false);
+
+                // Scroll to the top of the window
+                window.scroll(0, 0);
             },
             (error) => {
                 // Show error while fetching all the contacts
