e6de5503a98af9c9f8244bed66c5e31573e83954
ssonje <ssonje99@gmail.com>
2022-11-22 23:02:30 +0530

    JWT and swagger authorization

--- Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/config/swagger/SwaggerConfiguration.java
+++ Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/config/swagger/SwaggerConfiguration.java
@@ -1,6 +1,8 @@
 package com.smartcontactmanager.config.swagger;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,17 +10,38 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfiguration {
 
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
+	private ApiKey apiKeys() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+
+	private List<SecurityContext> securityContexts() {
+		return Arrays.asList(SecurityContext.builder().securityReferences(securityReferences()).build());
+	}
+
+	private List<SecurityReference> securityReferences() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("Global", "AccessEverything");
+		return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[] {authorizationScope}));
+	}
+
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(getInfo())
+				.securityContexts(securityContexts())
+				.securitySchemes(Arrays.asList(apiKeys()))
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
