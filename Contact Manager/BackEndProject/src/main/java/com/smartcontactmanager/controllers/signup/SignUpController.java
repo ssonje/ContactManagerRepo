3aba9bd7f39065abf38bb7b8f6375808fa6c3f99
ssonje <ssonje99@gmail.com>
2022-11-25 21:43:43 +0530

    Fixed Signup API

--- Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/controllers/signup/SignUpController.java
+++ Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/controllers/signup/SignUpController.java
@@ -3,15 +3,13 @@ package com.smartcontactmanager.controllers.signup;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.smartcontactmanager.entities.user.User;
+import com.smartcontactmanager.dto.UserDTO;
 import com.smartcontactmanager.payloads.apiResponse.APIResponse;
 import com.smartcontactmanager.services.signup.UserSignUpService;
 
@@ -22,10 +20,10 @@ public class SignUpController {
 	@Autowired
 	private UserSignUpService userSignUpService;
 
-	@RequestMapping(value = "/add/user" , method = RequestMethod.POST, consumes={ MediaType.MULTIPART_FORM_DATA_VALUE })
-	public ResponseEntity<?> addUser(@Valid @RequestPart User user, @RequestPart MultipartFile multipartFile) {
+	@PostMapping("/user")
+	public ResponseEntity<?> addUser(@Valid @RequestBody UserDTO userDTO) {
 		try {
-			APIResponse apiResponse = userSignUpService.addUser(user, multipartFile);
+			APIResponse apiResponse = userSignUpService.addUser(userDTO);
 			return ResponseEntity.ok(apiResponse);
 		} catch (Exception exception) {
 			APIResponse apiResponse = new APIResponse(exception.getMessage(), false);
--- Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/services/signup/UserSignUpService.java
+++ Contact Manager/BackEndProject/src/main/java/com/smartcontactmanager/services/signup/UserSignUpService.java
@@ -1,12 +1,10 @@
 package com.smartcontactmanager.services.signup;
 
-import org.springframework.web.multipart.MultipartFile;
-
-import com.smartcontactmanager.entities.user.User;
+import com.smartcontactmanager.dto.UserDTO;
 import com.smartcontactmanager.payloads.apiResponse.APIResponse;
 
 public interface UserSignUpService {
 
-	public APIResponse addUser(User user, MultipartFile multipartFile);
+	public APIResponse addUser(UserDTO userDTO);
 
 }
